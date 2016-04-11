67a236616c13a9bc49fe1125391697f3b21ab7cc
Michael Byrskog <micke884@hotmail.com>
2016-04-11 17:46:28 +0200

    cmit

--- IK075G/IK075G/MainMenu.Designer.cs
+++ IK075G/IK075G/MainMenu.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(196, 25);
+            this.label1.Location = new System.Drawing.Point(193, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 24);
             this.label1.TabIndex = 2;
