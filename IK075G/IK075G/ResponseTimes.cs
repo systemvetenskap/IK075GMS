1f8c6ab15822f1683e6f5fa1f49d587e6fee43e9
sumu1400 <sumu1400@student.miun.se>
2016-04-21 11:40:41 +0200

    Ändrart namn från ResponseTime till TimeMonitoring. Lagt till klassen ResponseTimes.

---
+++ IK075G/IK075G/ResponseTimes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IK075G
+{
+    public class ResponseTimes
+    {
+        public string customerGroup { get; set; }
+        public string prio { get; set; }
+        public string analys { get; set; }
+        public string year { get; set; }
+        public string month { get; set; }
+        public string week { get; set; }
+        public string day { get; set; }
+        public string hour { get; set; }
+        public string minute { get; set; }
+        public string quantity { get; set; }
+        public string avgTime { get; set; }
+        public string minTime { get; set; }
+        public string maxtime { get; set; }
+    }
+}
